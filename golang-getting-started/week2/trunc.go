8ab09232e224e96f1cf2f058e98a08f27391e4d7
Ken Ding <ken.ding@mail.com>
2020-10-01 02:10:44 +0930

    add week2 assignments

---
+++ golang-getting-started/week2/trunc.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+  )
+
+func main() {
+	var input float64
+
+	fmt.Print("Enter the number: ")
+	fmt.Scanln(&input)
+	int, _ := math.Modf(input)
+	fmt.Println("int part: ", int)
+}
